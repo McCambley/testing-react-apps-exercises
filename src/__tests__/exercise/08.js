7a592810c9d8df10d106d4197246816bef9e0a16
McCambley <jakemccamb16@gmail.com>
2024-09-29 08:58:46 -0400

    Refactor test to use ThemeProvider wrapper for rendering EasyButton and add error handling for geolocation API

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,17 +5,37 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-let result
-function TestComponent(props) {
-  result = useCounter(props)
-  return null
+function setup(...args) {
+  const returnVal = {}
+  function TestComponent() {
+    Object.assign(returnVal, useCounter(...args))
+    return null
+  }
+  render(<TestComponent />)
+  return returnVal
 }
 
-test('exposes the count and increment/decrement functions', () => {
-  render(<TestComponent />, {})
+test('allows customization of the initial count', () => {
+  const result = setup()
   expect(result.count).toEqual(0)
   act(() => {
     result.increment()
   })
   expect(result.count).toEqual(1)
+  act(() => {
+    result.decrement()
+  })
+  expect(result.count).toEqual(0)
+})
+test('allows customization of the step', () => {
+  const result = setup({step: 2})
+  expect(result.count).toEqual(0)
+  act(() => {
+    result.increment()
+  })
+  expect(result.count).toEqual(2)
+  act(() => {
+    result.decrement()
+  })
+  expect(result.count).toEqual(0)
 })

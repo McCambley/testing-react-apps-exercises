9d0a4242eda034110ce58f7ce35a120ba23a7bce
McCambley <jakemccamb16@gmail.com>
2024-09-29 08:46:31 -0400

    Refactor test to use ThemeProvider wrapper for rendering EasyButton and update useCounter test

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -6,35 +6,17 @@ import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-function TestComponent() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div title="div">{count}</div>
-      <button onClick={() => decrement()}>Decrement</button>
-      <button onClick={() => increment()}>Increment</button>
-    </div>
-  )
+let result
+function TestComponent(props) {
+  result = useCounter(props)
+  return null
 }
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
   render(<TestComponent />, {})
-  // 🐨 get the elements you need using screen
-  const button = screen.getByRole('button', {name: /decrement/i})
-  expect(button).toBeVisible()
-  // 🐨 assert on the initial state of the hook
-  const div = screen.getByTitle('div')
-  expect(div).toHaveTextContent('0')
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  expect(result.count).toEqual(0)
   act(() => {
-    button.click()
+    result.increment()
   })
-  expect(div).toHaveTextContent('-1')
+  expect(result.count).toEqual(1)
 })
-
-/* eslint no-unused-vars:0 */
